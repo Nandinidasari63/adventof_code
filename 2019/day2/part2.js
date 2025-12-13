ea1292d5b2d8ac07920d0f8a6432206a8247848a
Dasari Nandini <dasarinandini@Dasaris-MacBook-Pro.local>
2025-12-13 14:12:23 +0530

    my code before modification

---
+++ 2019/day2/part2.js
@@ -0,0 +1,35 @@
+  const alarm = (x) => {
+  const actual = x.map((a) => parseInt(a));
+  const y = actual.slice();
+  return process(y);
+};
+
+function process(dup) {
+  for (let index = 0; index < dup.length;) {
+    switch (dup[index]) {
+      case 1:
+        dup[dup[index + 3]] = dup[dup[index + 1]] + dup[dup[index + 2]];
+        break;
+      case 2:
+        dup[dup[index + 3]] = dup[dup[index + 1]] * dup[dup[index + 2]];
+        break;
+      case 99:
+        index = dup.length;
+        break;
+    }
+    index += 4;
+  }
+
+  if(dup[0] === 19690720){
+    console.log( dup);
+  }
+}
+
+for (let index = 0; index < 100; index++) {
+  for (let j = 0; j < 100; j++) {
+  const x = input.split(",");
+  x[1] = index;x[2] = j;
+  //console.log(x);
+    alarm(x);
+  }
+}
