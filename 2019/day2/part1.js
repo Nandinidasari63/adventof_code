23d5d430f447b08392cf3de423b04920d69ed8b4
Dasari Nandini <dasarinandini@Dasaris-MacBook-Pro.local>
2025-12-13 23:36:53 +0530

    added grid

--- 2019/day2/part1.js
+++ 2019/day2/part1.js
@@ -1,7 +1,27 @@
+import { chunk } from "jsr:@std/collections";
+
+const displayGrid = (computer) => {
+  const program = computer.program;
+
+  chunk(program, 12).forEach((row, rowIndex) => {
+    const styles = [];
+    const line = row.map((cell, colIndex) => {
+      const absoluteIndex = rowIndex * 12 + colIndex;
+      styles.push(
+        absoluteIndex === computer.index
+          ? "color:red;font-weight:bold"
+          : "color:green"
+      );
+      return "%c" + String(cell).padStart(7);
+    }).join(" ");
+    
+    console.log(line, ...styles);
+  });
+};
+
   const parseInput = (puzzleInput) => {
     const program = eval("[" + puzzleInput + "]");
     const computer = { program, index: 0, isHalted: false };
-    console.log(computer);
     return operationsOnProgram(computer);
   };
 
@@ -36,12 +56,12 @@
         2: performMul,
         99: performHalt,
       };
+      prompt('enter');
+      console.clear();
+      console.log(displayGrid(computer));
     operations[computer.program[computer.index]](computer);
     }
     return computer;
   }
-
-
-  // const displayGrid = (program) => {
-  // const 
-  // }
+const puzzleInput = Deno.readTextFileSync("input.txt");
+  console.log('last answer',parseInput(puzzleInput));
